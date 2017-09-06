5d276ec4f95c154eeeb42c1fb903c06330d7a184
sanskarsharma <sanskar2996@gmail.com>
2017-09-07 01:10:12 +0530

    main components added, project structuring done, header styled

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl:'./header.component.html',
+    selector: 'appHeader',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent{
+
+}
