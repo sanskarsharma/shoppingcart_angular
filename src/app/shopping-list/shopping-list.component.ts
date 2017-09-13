5c68e423f30f98fc01c7bf953dc8d15e75839299
sanskarsharma <sanskar2996@gmail.com>
2017-09-14 00:47:23 +0530

    styled and structured shopping list components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Ingredient} from '../common/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[]= [
+    new Ingredient("Apples",10),
+    new Ingredient("Mangoes", 12)
+  ];
 
   constructor() { }
 
